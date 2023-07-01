09b37828eba0858782c5a13dea75032f3b6f7ed8
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-01 16:39:35 -0700

    adds Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderImage from './HeaderImage';
+
+function Home() {
+  return (
+    <div className='home'>
+        <HeaderImage />
+    </div>
+  )
+}
+
+export default Home
