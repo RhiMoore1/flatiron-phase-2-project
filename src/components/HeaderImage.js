5350766347029a364192ecbd1c31f9a73c2e1710
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-01 16:36:46 -0700

    adds HeaderImage

---
+++ src/components/HeaderImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeaderImage() {
+  return (
+    <div className='header-Image-Container'>
+        <img className='headerImage' src='https://www.rd.com/wp-content/uploads/2020/05/bermuda-crystal-caves.jpg?w=3914' />
+    </div>
+  )
+}
+
+export default HeaderImage
