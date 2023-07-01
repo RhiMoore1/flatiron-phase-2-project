68670d3f7d7f483dbb22b6042a10b553e024f26b
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-01 16:52:47 -0700

    adds Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 
 
 function App() {
+  const [page, setPage] = useState("/home");
+
   return (
     <div className="App">
-      <NavBar />
-      <Home />
+      <NavBar onChangePage={setPage} />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/explore">
+          <CaveContainer />
+        </Route>
+        <Route path="/add">
+          <AddNew />
+        </Route>
+      </Switch>
     </div>
   );
 }
