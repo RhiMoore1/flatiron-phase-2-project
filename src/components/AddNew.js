044850eac5b31595f3e471747c5902c9af42b170
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-01 16:57:53 -0700

    adds AddNew file for form to submit new cave

--- src/components/AddNew.js
+++ src/components/AddNew.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function AddNew() {
+
+
+function AddNew({ onAddCave }) {
+    const [caveName, setCaveName] = useState("");
+    const [caveLocation, setCaveLocation] = useState("");
+    const [caveRegion, setCaveRegion] = useState("Northern");
+    const [caveDescription, setCaveDescription] = useState("");
+    const [caveImage, setCaveImage] = useState("");
+
   return (
-    <div>AddNew</div>
+    <div className='form-container'>
+        <form className='form' >
+            <h1>Add New Cave</h1>
+
+            <label htmlFor="caveName">Cave Name</label>
+            <input
+                type='text' 
+                id='caveName' 
+                value={caveName} 
+                onChange={(e) => setCaveName(e.target.value)}
+            ></input>
+
+            <label htmlFor="caveLocation">Cave Location</label>
+            <input 
+                type='text' 
+                id='caveLocation'
+                value={caveLocation}
+                onChange={(e) => setCaveLocation(e.target.value)}
+                ></input>
+
+            <label htmlFor="caveRegion">Region</label>
+            <select 
+                id='caveRegion'
+                value={caveRegion}
+                onChange={(e) => setCaveRegion(e.target.value)}f
+                >
+                    <option value="northern">Northern</option>
+                    <option value="souther">Southern</option>
+                    <option value="eastern">Eastern</option>
+                    <option value="western">Western</option>
+                    <option value="midwest">Midwest</option>
+            </select>
+
+            <label htmlFor="caveDescription">Cave Description</label>
+            <textarea
+                type='text' 
+                id='caveDescription'
+                value={caveDescription}
+                onChange={(e) => setCaveDescription(e.target.value)}
+            ></textarea>
+
+            <label htmlFor="caveImage">Cave Image</label>
+            <input 
+            type='img' 
+            id='caveImage'
+            value={caveImage}
+            onChange={(e) => setCaveImage(e.target.value)}
+            ></input>
+
+
+            <input type="submit" name="addCave" id="addCave" value="Add Cave"/>
+        </form>
+    </div>
   )
 }
+
+export default AddNew
