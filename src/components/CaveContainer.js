3e35659c080b8c9ffd4ea1376fcd08f8c48a92c4
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-01 18:48:09 -0700

    adds a filter by region option to the CaveContainer.js

--- src/components/CaveContainer.js
+++ src/components/CaveContainer.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CaveCard from './CaveCard';
 import '../components/CaveContainer.css'
 
 function CaveContainer({caves}) {
+    const [selectedRegion, setSelectedRegion] = useState("");
+
+    const handleSelectedRegionChange = (e) => {
+        setSelectedRegion(e.target.value)
+    };
+
+    const filteredCaves = selectedRegion
+        ? caves.filter((cave) => cave.region === selectedRegion)
+        : caves;
 
     return (
     <div className='caves'>
         <div className='caves-list'>
-            {caves.map((cave) => {
-                return (
-                    <CaveCard 
-                    key={cave.id} 
-                    image={cave.image} 
-                    title={cave.title} 
-                    description={cave.description}
-                    region={cave.region}
-                    location={cave.location}                    
-                    />
-                )
-            })}
+            <div className='region-filter'>
+                <label htmlFor='region'>Filter by Region</label>
+                <select id='region' value={selectedRegion} onChange={handleSelectedRegionChange} >
+                    <option value=''>All Regions</option>
+                    <option value='Northern'>Northern</option>
+                    <option value='Southern'>Southern</option>
+                    <option value='Eastern'>Easterrn</option>
+                    <option value='Western'>Western</option>
+                    <option value='Midwest'>Midwest</option>
+                </select>
+            </div>
+                {filteredCaves.map((cave) => {
+                    return (
+                        <CaveCard 
+                        key={cave.id} 
+                        image={cave.image} 
+                        title={cave.title} 
+                        description={cave.description}
+                        region={cave.region}
+                        location={cave.location}                    
+                        />
+                    )
+                })}
         </div>
     </div>
   )
