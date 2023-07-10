b53fa5243580ae60b003ff6e25026b80028d3009
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-10 09:37:27 -0700

    updates caveCard emoji styles

--- src/components/CaveCard.js
+++ src/components/CaveCard.js
@@ -6,7 +6,14 @@ function CaveCard({ title, image, description, region, location }) {
   return (
     <div className='toplevel'>
     <div className='cavecard'>
+      <div className='card-heading'>
         <h3>{title}</h3>
+        <div className='card-buttons'>
+          <button className='button-grow'>⛏️</button>
+          <button className='button-grow'>✅</button>
+          <button className='button-grow'>❤️</button>
+        </div>
+      </div>
         <img className="caveCardImage" src={image} alt='cave'/>
         <div className='loction-region'>
           <p>{location}</p>
